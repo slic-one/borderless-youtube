f21590c56fcc818b4bb34f254a9a1d90a18a0c89
slic <slic1one@gmail.com>
2018-08-18 16:35:24 +0300

    menu toggle basic functionality

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 // get webview and insert custom CSS
 
-// const webview = $('#player');
 const webview = document.querySelector('webview');
-// let isMenuOpen = false;
+let isMenuOpen = false;
 
 webview.addEventListener('did-finish-load', () => {
-    webview.insertCSS(
-        '#masthead-container.ytd-app{position:absolute;top:1000px;}'
+    const webview = document.getElementById('player');
+
+
+    // TODO rewrite this
+    const hiddenMenuStyle = '#masthead-container.ytd-app{position:absolute;top:1000px;}'
         + '#page-manager.ytd-app{margin-top:0;}'
         + '#top{margin-top:0}'
-        + 'ytd-watch-flexy:not([theater]):not([fullscreen]) #primary.ytd-watch-flexy, ytd-watch-flexy:not([theater]):not([fullscreen]) #secondary.ytd-watch-flexy{padding-top:0}'
-    );
+        + 'ytd-watch-flexy:not([theater]):not([fullscreen]) #primary.ytd-watch-flexy, ytd-watch-flexy:not([theater]):not([fullscreen]) #secondary.ytd-watch-flexy{padding-top:0}';
+    const defaultMenuStyle = '#masthead-container.ytd-app{position:relative;top:0;}'
+        + '#page-manager.ytd-app{margin-top:0;}'
+        + '#top{margin-top:0}'
+        + 'ytd-watch-flexy:not([theater]):not([fullscreen]) #primary.ytd-watch-flexy, ytd-watch-flexy:not([theater]):not([fullscreen]) #secondary.ytd-watch-flexy{padding-top:0}';
 
-    // $('#menu-toggle').on('click', () => {
-    //     console.log('togglin')
-    //     if (isMenuOpen) {
-    //         webview.insertCss('#masthead:{display:none;}');
-    //     } else {
-    //         webview.insertCss('#masthead:{display:block;}');
-    //     }
-    //     isMenuOpen = !isMenuOpen;
-    // })
+    document.getElementById('menu-toggle').onclick = () => {
+        if (isMenuOpen) {
+            webview.executeJavaScript('var sheet = document.createElement("style");sheet.innerHTML = "' + hiddenMenuStyle + '";document.body.appendChild(sheet);');
+        } else {
+            webview.executeJavaScript('var sheet = document.createElement("style");sheet.innerHTML = "' + defaultMenuStyle + '";document.body.appendChild(sheet);');
+        }
+    };
 })
 
